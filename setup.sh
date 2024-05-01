987d52b5ce2d1c5585ba1ff70739bca494191c00
Fabio Azevedo (CTW) <Fabio.AM.Azevedo@ctw.bmwgroup.com>
2024-05-02 00:20:10 +0100

    Changes to structure

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ mkdir -p "$XDG_CONFIG_HOME"/alacritty
 
 # Symbolic links
 ln -sf "$PWD/alacritty.toml" "$XDG_CONFIG_HOME"/alacritty/alacritty.toml
-ln -sf "$PWD/.zshrc" "$HOME"/.zshrc
+ln -sf "$PWD/zshrc/.zshrc" "$HOME"/.zshrc
