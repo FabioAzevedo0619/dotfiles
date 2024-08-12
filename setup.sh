adbf216e6deb7087b82806cc1ed70dd3f815e9a7
Fabio Azevedo (CTW) <Fabio.AM.Azevedo@ctw.bmwgroup.com>
2024-08-12 12:05:05 +0100

    Added hability to create kitty config file

--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@
 # Create config directories
 export XDG_CONFIG_HOME="$HOME"/.config
 mkdir -p "$XDG_CONFIG_HOME"/alacritty
+mkdir -p "$XDG_CONFIG_HOME"/kitty
 
 # Symbolic links
 ln -sf "$PWD/alacritty.toml" "$XDG_CONFIG_HOME"/alacritty/alacritty.toml
+ln -sf "$PWD/kitty.conf" "$XDG_CONFIG_HOME"/kitty/kitty.conf
 ln -sf "$PWD/zshrc/.zshrc" "$HOME"/.zshrc
